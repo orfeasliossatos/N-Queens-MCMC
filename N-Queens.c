4ed6701fd9e38da41ebd4aedb6eb3182ac53871e
grennault <gaietan.renault@epfl.ch>
2022-12-19 02:43:20 +0100

    Auto format, add possibility to repeat

--- N-Queens.c
+++ N-Queens.c
@@ -7,9 +7,10 @@
 /**
  * Fills in z with integers from min to max by step inplace.
  */
-void arange(int* z, int min, int max, int step) 
-{	
-	for (int i = 0; i < max-min; i += step) {
+void arange(int *z, int min, int max, int step)
+{
+	for (int i = 0; i < max - min; i += step)
+	{
 		z[i] = min + i;
 	}
 }
@@ -17,10 +18,11 @@ void arange(int* z, int min, int max, int step)
 /**
  * Prints an array of integers
  */
-void print_int_arr(int* z, int length)
+void print_int_arr(int *z, int length)
 {
 	printf("[ ");
-	for (int i = 0; i < length; i++) {
+	for (int i = 0; i < length; i++)
+	{
 		printf("%d ", z[i]);
 	}
 	printf("]\n");
@@ -29,20 +31,20 @@ void print_int_arr(int* z, int length)
 /**
  * Prints an array of doubles
  */
-void print_dbl_arr(double* z, int length)
+void print_dbl_arr(double *z, int length)
 {
 	printf("[ ");
-	for (int i = 0; i < length; i++) {
+	for (int i = 0; i < length; i++)
+	{
 		printf("%lf.3 ", z[i]);
 	}
 	printf("]\n");
 }
 
-
 /**
  * Swaps two array indices inplace.
  */
-void swap(int* z, int i, int j) 
+void swap(int *z, int i, int j)
 {
 	int t = z[i];
 	z[i] = z[j];
@@ -52,11 +54,13 @@ void swap(int* z, int i, int j)
 /**
  * Calculates number of unique threats on board.
  */
-int loss(int* z, int* idx_pairs, int C) 
+int loss(int *z, int *idx_pairs, int C)
 {
 	int sum = 0;
-	for (int k = 0; k < 2 * C; k += 2) {
-		int i = idx_pairs[k]; int j = idx_pairs[k+1];
+	for (int k = 0; k < 2 * C; k += 2)
+	{
+		int i = idx_pairs[k];
+		int j = idx_pairs[k + 1];
 		sum += (j - i == abs(z[j] - z[i]));
 	}
 	return sum;
@@ -66,13 +70,15 @@ int loss(int* z, int* idx_pairs, int C)
  * Calculates all pairwise combinations of indices less than N
  * and stores them contiguously in dst.
  */
-void pairs(int* dst, int N) 
+void pairs(int *dst, int N)
 {
 	int k = 0;
-	for (int i = 0; i < N-1; i++) {
-		for (int j = i+1; j < N; j++) {
+	for (int i = 0; i < N - 1; i++)
+	{
+		for (int j = i + 1; j < N; j++)
+		{
 			dst[k] = i;
-			dst[k+1] = j;
+			dst[k + 1] = j;
 			k += 2;
 		}
 	}
@@ -81,24 +87,28 @@ void pairs(int* dst, int N)
 /**
  * Returns the minimum of two doubles.
  */
-double min(double a, double b) {
-	return a*(a<b) + b*(a>=b);
+double min(double a, double b)
+{
+	return a * (a < b) + b * (a >= b);
 }
 
 /**
  * Returns the max of two doubles.
  */
-double max(double a, double b) {
-	return a*(a>b) + b*(a<=b);
+double max(double a, double b)
+{
+	return a * (a > b) + b * (a <= b);
 }
 
 /**
  * Returns number of queens threatening queen i.
  */
-int threats(int* z, int i, int N) {
+int threats(int *z, int i, int N)
+{
 	int sum = 0;
-	for (int k = 0; k < N; k++) {
-		sum += abs(k-i) == abs(z[k]-z[i]);
+	for (int k = 0; k < N; k++)
+	{
+		sum += abs(k - i) == abs(z[k] - z[i]);
 	}
 	sum -= 1;
 	return sum;
@@ -107,7 +117,8 @@ int threats(int* z, int i, int N) {
 /**
  * One-step loss difference.
  */
-int loss_diff(int* z, int i, int j, int N) {
+int loss_diff(int *z, int i, int j, int N)
+{
 	int old = threats(z, i, N) + threats(z, j, N);
 	swap(z, i, j);
 	int new = threats(z, i, N) + threats(z, j, N);
@@ -118,14 +129,19 @@ int loss_diff(int* z, int i, int j, int N) {
 /**
  * Sample from the distribution p with support size X.
  */
-int sample(double *p, int X) {
-	
-	double r = (double) rand() / RAND_MAX;
+int sample(double *p, int X)
+{
+
+	double r = (double)rand() / RAND_MAX;
 	double F = 0;
 	int k = 0;
-	for (; k < X; k++) {
+	for (; k < X; k++)
+	{
 		F += p[k];
-		if (r <= F) { break; }
+		if (r <= F)
+		{
+			break;
+		}
 	}
 	return k;
 }
@@ -133,23 +149,25 @@ int sample(double *p, int X) {
 /**
  * Perform K random swaps on a list of integers.
  */
-void shuffle(int* z, int K, int* idx_pairs, int nb_pairs) 
+void shuffle(int *z, int K, int *idx_pairs, int nb_pairs)
 {
-	for (int k = 0; k < K; k++) {
+	for (int k = 0; k < K; k++)
+	{
 		int pair_idx = (rand() % nb_pairs) * 2;
 		int i = idx_pairs[pair_idx];
-		int j = idx_pairs[pair_idx+1];
+		int j = idx_pairs[pair_idx + 1];
 		swap(z, i, j);
 	}
 }
 
 /**
- * Assumes memory has been allocated. Copies len elements of 
+ * Assumes memory has been allocated. Copies len elements of
  * src into dst.
  */
-void arrcopy(int* src, int* dst, int len) 
+void arrcopy(int *src, int *dst, int len)
 {
-	for (int i = 0; i < len; i++) {
+	for (int i = 0; i < len; i++)
+	{
 		dst[i] = src[i];
 	}
 }
@@ -157,21 +175,22 @@ void arrcopy(int* src, int* dst, int len)
 /**
  * Takes an integer array src an prints src[i], i for i in range(len)
  */
-void printcsv(int* src, int len) {
+void printcsv(int *src, int len)
+{
 	// Save to CSV
 	FILE *fp;
-	
+
 	fp = fopen("N_Queens_solution.csv", "w");
-	
-	if(fp == NULL) {
-        printf("file can't be opened\n");
-        exit(1);
-    }
- 
-	
-	for (int i=0; i < len; i++) {
-		fprintf(fp,"%i,%i\n", src[i], i);
-		
+
+	if (fp == NULL)
+	{
+		printf("file can't be opened\n");
+		exit(1);
+	}
+
+	for (int i = 0; i < len; i++)
+	{
+		fprintf(fp, "%i,%i\n", src[i], i);
 	}
 }
 
@@ -179,129 +198,158 @@ void printcsv(int* src, int len) {
  * Saves int array from files with format
  * a1,a2,a3,a4,a5
  */
-void save_lf_arr(char* filename, int *src, int len) {
-	
-	FILE *fp = fopen(filename, "w");
-	if(fp == NULL) {
-        printf("file can't be opened\n");
-        exit(1);
-    }
-    
-    for (int i = 0; i < len; i++) {
-		if (i != len-1) {
+void save_lf_arr(char *filename, int *src, int len)
+{
+
+	FILE *fp = fopen(filename, "a+"); // TODO: Append
+	if (fp == NULL)
+	{
+		printf("file can't be opened\n");
+		exit(1);
+	}
+
+	for (int i = 0; i < len; i++)
+	{
+		if (i != len - 1)
+		{
 			fprintf(fp, "%d,", src[i]);
-		} else {
-			fprintf(fp, "%d", src[i]);
+		}
+		else
+		{
+			fprintf(fp, "%d\n", src[i]); // TODO: New line
 		}
 	}
+	fclose(fp);
 }
 
 /**
  * Saves and appends int to file with format
  * a1,a2,a3,a4,a5
  */
-void append_int(char* filename, int src, int len) {
-	
+void append_int(char *filename, int src, int len)
+{
 	FILE *fp = fopen(filename, "aw");
-	if(fp == NULL) {
-        printf("file can't be opened\n");
-        exit(1);
-    }
+	if (fp == NULL)
+	{
+		printf("file can't be opened\n");
+		exit(1);
+	}
 
-	fprintf(fp, "%d,", src)
+	fprintf(fp, "%d,", src);
 }
 
 int main()
-{	
-	// Snapshot of time
-	time_t then;
-	time(&then);
-	
+{
 	// Reproducibility
 	srand(2022);
-	
-	// Variables
-	int N = 1000;
-	int C = N * (N-1) / 2;
-	double b = 100;
-	
-	// Starting position
-	int *z = malloc(N * sizeof(int));
-	arange(z, 1, N+1, 1);
-	
-	// Pairs of queen indices
-	int *idx_pairs = malloc(2 * C * sizeof(int));
-	pairs(idx_pairs, N);
-	
-	// Shuffle the starting position
-	shuffle(z, 2 * N, idx_pairs, C);
-		
-	// Search
-	int t = 0;
-	int MAX_ITERS = 10000000;
-	
-	// Calculate current loss
-	int l = loss(z, idx_pairs, C);
 
-	
+	// Number of repeat
+	int repeat = 10;
 
-	int *losses = malloc((MAX_ITERS) * sizeof(int));
-	
-	for (t = 1; t < MAX_ITERS; t++) {
-		
-		// b = log(t*t/N);
-		int k = (rand() % C);
-		int i = idx_pairs[2*k]; 
-		int j = idx_pairs[2*k+1];
-		double diff = (double)loss_diff(z, i, j, N);
-		
-		double acc = (diff < 0) ? 1 : exp(-b*diff);
-		double r = ((double)rand() / (double)(RAND_MAX));
-		if (r < acc) {
-			swap(z, i, j);
-			l += diff;
-			
+	FILE *fp = fclose(fopen("losses.csv", "w")); // TODO: Clear file contents
+
+	for (int repeat_nbr = 0; repeat_nbr < repeat; repeat_nbr++)
+	{
+		// Snapshot of time
+		time_t then;
+		time(&then);
+
+		// Variables
+		int N = 1000;
+		int C = N * (N - 1) / 2;
+		double b = 100;
+
+		// Starting position
+		int *z = malloc(N * sizeof(int));
+		arange(z, 1, N + 1, 1);
+
+		// Pairs of queen indices
+		int *idx_pairs = malloc(2 * C * sizeof(int));
+		pairs(idx_pairs, N);
+
+		// Shuffle the starting position
+		shuffle(z, 2 * N, idx_pairs, C);
+
+		// Search
+		int t = 0;
+		int MAX_ITERS = 500000;
+
+		// Calculate current loss
+		int l = loss(z, idx_pairs, C);
+
+		int *losses = malloc((MAX_ITERS) * sizeof(int));
+
+		for (t = 1; t <= MAX_ITERS; t++)
+		{
+			// beta to change
+			// b = t;
+			// b = log(t + 10) * (10 * N);
+			// b = log(t);
+			// b = log(t * t);
+			b = t * t;
+			int k = (rand() % C);
+			int i = idx_pairs[2 * k];
+			int j = idx_pairs[2 * k + 1];
+			double diff = (double)loss_diff(z, i, j, N);
+
+			double acc = (diff < 0) ? 1 : exp(-b * diff);
+			double r = ((double)rand() / (double)(RAND_MAX));
+			if (r < acc)
+			{
+				swap(z, i, j);
+				l += diff;
+			}
+
+			losses[t - 1] = (int)l;
+
+			/* UNCOMMENT TO PRINT LOSS EVOLUTION
+			if (t % 1000 == 0)
+			{
+				printf("current loss : %d,\t diff : %lf,\t acc: %lf%%\n", l, diff, acc);
+			}
+			*/
+
+			if (l == 0)
+			{
+				break;
+			}
 		}
 
+		// Save losses
+		save_lf_arr("losses.csv", losses, MAX_ITERS);
 
-		losses[t-1] = l;
-		
-		/* UNCOMMENT TO PRINT LOSS EVOLUTION
-		if (t % 1000 == 0) {
-			printf("current loss : %d,\t diff : %lf,\t acc: %lf%%\n", l, diff, acc);
-		}*/
-		
-		if (l == 0) { break; }
+		// Results
+		printf("\nFor the %d - queens problem\n----------------\n", N);
+
+		if (loss(z, idx_pairs, C) == 0)
+		{
+			printf("Solution found in %d iterations:\n", t);
+			print_int_arr(z, N);
+		}
+		else
+		{
+			printf("Solution not found in %d iterations\n", MAX_ITERS);
+			print_int_arr(z, N);
+		}
+
+		// print to csv
+		printcsv(z, N);
+
+		// Timing information
+		time_t now;
+		time(&now);
+
+		time_t diff = now - then;
+
+		printf("Began: %s", ctime(&then));
+		printf("Ended: %s", ctime(&now));
+		printf("Diff: %d minutes, %d seconds\n", diff / 60, diff % 60);
+
+		// Free memory
+		free(z);
+		free(idx_pairs);
+		free(losses);
 	}
 
-	save_lf_arr("losses.csv", losses, MAX_ITERS);
-	
-	// Results
-	printf("\nFor the %d - queens problem\n----------------\n", N);
-	
-	if (loss(z, idx_pairs, C) == 0) {
-		printf("Solution found in %d iterations:\n", t);
-		print_int_arr(z, N);
-	} else {
-		printf("Solution not found in %d iterations\n", MAX_ITERS);
-		print_int_arr(z, N);
-	}
- 
-	
-	// print to csv
-	printcsv(z, N);
-	
-	// Timing information
-	time_t now;
-	time(&now);
-	
-	time_t diff = now - then;
-	
-	printf("Began: %s", ctime(&then));
-	printf("Ended: %s", ctime(&now));
-	printf("Diff: %d minutes, %d seconds", diff / 60, diff % 60);
-	
-	free(z);
-	free(idx_pairs);
 	return 0;
 }
